332999f0d9291be6e0ab22094bee5032dad4de20
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-04-10 21:40:59 -0600

    Add button theme component

---
+++ src/components/ButtonTheme.jsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../hooks/ThemeProvider'
+
+const ButtonTheme =() => {
+    const {changeTheme, darkMode} = useContext(ThemeContext);
+
+  return (
+    <button className={darkMode?"dark":"normal"} onClick={changeTheme}>Change Theme</button>
+  )
+}
+
+export default ButtonTheme
