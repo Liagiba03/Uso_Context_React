355cfcf4af0f3280d533acdfb7b45bab01d2633b
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-04-14 11:13:52 -0600

    Add empty code changes for future development

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,6 @@ import { ThemeContext } from '../hooks/ThemeProvider'
 
 function Title() {
 
-  const {changeTheme, darkMode} = useContext(ThemeContext);
   return (
     <h1 className={darkMode?"h1-dark":"h1-normal"}>Title</h1>
   )
